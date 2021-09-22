0d180942a30ce652c86e32dad38c9f2464d7a70b
Ayon95 <mushfiqurrahman78@yahoo.com>
2021-09-22 18:02:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# EasyAppoint
+
+An appointment-management app made with React, Material UI, Express, and MySQL
---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>EasyAppoint</h1>
+    </div>
+  );
+}
+
+export default App;
