47e2f584fe82bee3bcb019a0f58aa2464d4221ab
Ayon95 <mushfiqurrahman78@yahoo.com>
2021-09-28 13:53:37 -0400

    client - in appointmentService.js, added a function that will fetch appointments from the server, defined a query for that, and added pagination code in AppointmentTable.js

--- client/src/components/AppointmentTable/AppointmentTable.js
+++ client/src/components/AppointmentTable/AppointmentTable.js
@@ -1,5 +1,7 @@
-import { Table, TableContainer } from '@mui/material';
-import React from 'react';
+import { Paper, Table, TableContainer, TablePagination } from '@mui/material';
+import React, { useState } from 'react';
+import { useQuery, useQueryClient } from 'react-query';
+import { getAppointments } from '../../services/appointmentService';
 import TableBodyComponent from './TableBodyComponent';
 import TableHeadComponent from './TableHeadComponent';
 
@@ -25,13 +27,51 @@ const appointments = [
 ];
 
 function AppointmentTable() {
+	const queryClient = useQueryClient();
+	// variables related to pagination
+	const [page, setPage] = useState(1);
+	const [rowsPerPage, setRowsPerPage] = useState(25);
+	const rowsPerPageOptions = [25, 50, 100];
+	// query that will be responsible for fetching appointments from the server
+	const requestData = { page, rowsPerPage };
+	// the returned data will be an object that looks like this -> {totalAppointments: , appointments: [...]}
+	const { data } = useQuery(['appointments', requestData], () => getAppointments(requestData));
+
+	// the second argument is the zero-based index of the current page
+	function handleChangePage(_, pageIndex) {
+		setPage(pageIndex + 1);
+		// when the user changes the page, we have to invalidate the appointments query so that it refetches new data
+		queryClient.invalidateQueries('appointments');
+	}
+
+	function handleChangeRowsPerPage(event) {
+		setRowsPerPage(Number.parseFloat(event.target.result));
+		// We want to go back to the first page
+		/* Let's say, a table has 25 rows in total and currently each page shows 5 rows, so there are 5 pages.
+		Suppose we are on the 5th page and we changed rows per page to 25. If we don't go back to the first page,
+		then page 5 will simply be empty since now each page is showing 25 rows */
+		setPage(1);
+		// when the user changes number of rows per page, we have to invalidate the appointments query so that it refetches new data
+		queryClient.invalidateQueries('appointments');
+	}
 	return (
-		<TableContainer>
-			<Table>
-				<TableHeadComponent columns={columns} />
-				<TableBodyComponent appointments={appointments} />
-			</Table>
-		</TableContainer>
+		<Paper>
+			<TableContainer>
+				<Table>
+					<TableHeadComponent columns={columns} />
+					<TableBodyComponent appointments={data.appointments} />
+				</Table>
+			</TableContainer>
+			<TablePagination
+				component="div"
+				count={data.totalAppointments}
+				page={page - 1}
+				rowsPerPage={rowsPerPage}
+				rowsPerPageOptions={rowsPerPageOptions}
+				onChangePage={handleChangePage}
+				onChangeRowsPerPage={handleChangeRowsPerPage}
+			/>
+		</Paper>
 	);
 }
 
--- client/src/services/appointmentService.js
+++ client/src/services/appointmentService.js
@@ -1,5 +1,22 @@
 const baseUrl = 'http://localhost:5000/appointments';
 
+// this function will send a GET request to the server to fetch appointments
+export async function getAppointments(requestData) {
+	const response = await fetch(
+		`${baseUrl}?page=${requestData.page}&rows_per_page=${requestData.rowsPerPage}`
+	);
+
+	if (!response.ok) {
+		// the server will respond with an error object containing an error message
+		const error = await response.json();
+		// this error can be accessed in the onError callback of React query
+		throw new Error(error.errorMessage);
+	}
+
+	const data = await response.json();
+	return data;
+}
+
 // this function will send a POST request to the server for adding an appointment
 export async function addAppointment(requestData) {
 	const response = await fetch(baseUrl, {
@@ -12,9 +29,7 @@ export async function addAppointment(requestData) {
 	});
 
 	if (!response.ok) {
-		// the server will respond with an error object containing an error message
 		const error = await response.json();
-		// this error can be accessed in the onError callback of React query
 		throw new Error(error.errorMessage);
 	}
 
